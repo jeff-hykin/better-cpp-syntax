480f284068da0efe9d3ca62c6913c8747b974d29
jeff-hykin <jeff.hykin@gmail.com>
2019-02-18 17:28:33 -0600

    improve the removal of default mode modifiers

--- readable_regex.rb
+++ readable_regex.rb
@@ -1,65 +1,71 @@
 # extend Regexp to make expressions very readable
 class Regexp
     # convert it to a string and have it without the "(?-mix )" part
-    def as_readable_string()
-        return self.to_s.sub(/\A\(\?\-mix\:/, "").sub(/\)\z/,"")
+    def remove_default_mode_modifiers()
+        as_string = self.to_s
+        # if it is the default settings (AKA -mix) then remove it
+        if (as_string.size > 6) and (as_string[0..5] == '(?-mix')
+            return as_string.sub(/\A\(\?\-mix\:/, "").sub(/\)\z/,"")
+        else 
+            return as_string
+        end
     end
-    # an alias operator for "as_readable_string"
+    # an alias operator for "remove_default_mode_modifiers"
     def -@()
-        return self.as_readable_string
+        return self.remove_default_mode_modifiers
     end
     # 
     # English Helpers
     # 
     def or(other_regex)
-        return /(?:(?:#{self.as_readable_string}|#{other_regex.as_readable_string}))/
+        return /(?:(?:#{self.remove_default_mode_modifiers}|#{other_regex.remove_default_mode_modifiers}))/
     end
     def and(other_regex)
-        return /#{self.as_readable_string}#{other_regex.as_readable_string}/
+        return /#{self.remove_default_mode_modifiers}#{other_regex.remove_default_mode_modifiers}/
     end
     def then(other_regex)
         return self.and(other_regex)
     end
     def lookAheadFor(other_regex)
-        return /#{self.as_readable_string}(?=#{other_regex.as_readable_string})/
+        return /#{self.remove_default_mode_modifiers}(?=#{other_regex.remove_default_mode_modifiers})/
     end
     def lookAheadToAvoid(other_regex)
-        return /#{self.as_readable_string}(?!#{other_regex.as_readable_string})/
+        return /#{self.remove_default_mode_modifiers}(?!#{other_regex.remove_default_mode_modifiers})/
     end
     def lookBehindFor(other_regex)
-        return /#{self.as_readable_string}(?<=#{other_regex.as_readable_string})/
+        return /#{self.remove_default_mode_modifiers}(?<=#{other_regex.remove_default_mode_modifiers})/
     end
     def lookBehindToAvoid(other_regex)
-        return /#{self.as_readable_string}(?<!#{other_regex.as_readable_string})/
+        return /#{self.remove_default_mode_modifiers}(?<!#{other_regex.remove_default_mode_modifiers})/
     end
     def thenNewGroup(name_or_regex, regex_pattern=nil)
         # numbered group
         if regex_pattern == nil
-            return /#{self.as_readable_string}(#{name_or_regex.as_readable_string})/
+            return /#{self.remove_default_mode_modifiers}(#{name_or_regex.remove_default_mode_modifiers})/
         # named group
         else
-            return /#{self.as_readable_string}(?<#{name_or_regex}>#{regex_pattern.as_readable_string})/
+            return /#{self.remove_default_mode_modifiers}(?<#{name_or_regex}>#{regex_pattern.remove_default_mode_modifiers})/
         end
     end
     def thenNewHiddenGroup(other_regex)
-        return /#{self.as_readable_string}(?:#{other_regex.as_readable_string})/
+        return /#{self.remove_default_mode_modifiers}(?:#{other_regex.remove_default_mode_modifiers})/
     end
     def maybe(other_regex)
-        regex_as_string = other_regex.as_readable_string
+        regex_as_string = other_regex.remove_default_mode_modifiers
         # if its already a + character
         if (regex_as_string.size == 3) and regex_as_string[0] == "\\" and regex_as_string[-1] == "+"
-            return /#{self.as_readable_string}#{regex_as_string[0] + regex_as_string[1]}*/
+            return /#{self.remove_default_mode_modifiers}#{regex_as_string[0] + regex_as_string[1]}*/
         elsif (regex_as_string.size == 2) and regex_as_string[0] != "\\" and regex_as_string[-1] == "+"
-            return /#{self.as_readable_string}#{regex_as_string[0]}*/
+            return /#{self.remove_default_mode_modifiers}#{regex_as_string[0]}*/
         else
-            return /#{self.as_readable_string}(?:#{regex_as_string})?/
+            return /#{self.remove_default_mode_modifiers}(?:#{regex_as_string})?/
         end
     end
     def oneOrMoreOf(other_regex)
-        return /#{self.as_readable_string}(?:#{other_regex.as_readable_string})+/
+        return /#{self.remove_default_mode_modifiers}(?:#{other_regex.remove_default_mode_modifiers})+/
     end
     def zeroOrMoreOf(other_regex)
-        return /#{self.as_readable_string}(?:#{other_regex.as_readable_string})*/
+        return /#{self.remove_default_mode_modifiers}(?:#{other_regex.remove_default_mode_modifiers})*/
     end
 end
 
@@ -99,13 +105,13 @@ end
 # Make safe failure for regex methods on strings
 # 
 class String 
-    # make the as_readable_string do nothing for strings
-    def as_readable_string()
+    # make the remove_default_mode_modifiers do nothing for strings
+    def remove_default_mode_modifiers()
         return self
     end
-    # an alias operator for "as_readable_string"
+    # an alias operator for "remove_default_mode_modifiers"
     def -@()
-        return self.as_readable_string
+        return self.remove_default_mode_modifiers
     end
 end
 
