348c12edd77acb87bdc7bc2faf299e4f2e6bb4c1
Jeff Hykin <jeff.hykin@gmail.com>
2019-04-24 14:40:53 -0500

    _fix language constants

--- cpp/generate.rb
+++ cpp/generate.rb
@@ -101,7 +101,7 @@ cpp_grammar = Grammar.new(
             :other_keywords,
             :memory_operators,
             :the_this_keyword,
-            :language_constants,
+            :constants,
             :template_isolated_definition,
             :template_definition,
             :scope_resolution,
