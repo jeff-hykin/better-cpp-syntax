9467d496d115e7040cb9cd792e35ca67ce9e4121
Matthew Fosdick <matthew.fosdick@gmail.com>
2019-05-10 11:17:13 -0700

    add multi word types to fixture for issue 71.

--- test/fixtures/issues/071.cpp
+++ test/fixtures/issues/071.cpp
@@ -1,11 +1,11 @@
 struct A {
-    int a;
-    int b;
-    usertype user_value;
-    usertype user_value{'A'};
-    int a[] = {1,2,3};
-    scoped::templated<type> value;
-    compound<typename templated::type *>::type value;
+	int a;
+	int b;
+	usertype user_value;
+	usertype user_value{'A'};
+	int a[] = {1, 2, 3};
+	scoped::templated<type> value;
+	compound<typename templated::type *>::type value;
 };
 
 int a;
@@ -14,5 +14,7 @@ usertype user_value;
 usertype user_value{'A'};
 scoped::templated<type> value;
 compound<typename templated::type *>::type value;
-int a[] = {1,2,3};
+int a[] = {1, 2, 3};
 void function_declaration(int *, usertype user_value);
+void multi_word_types(signed char, unsigned char, short int, long int, long long,
+                      long double, struct a, enum b);
