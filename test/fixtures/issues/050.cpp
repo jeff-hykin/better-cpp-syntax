48b13c48d98eaf3e81bd6f895ea601ccbb83ef8b
Jeff Hykin <jeff.hykin@gmail.com>
2019-05-10 21:54:38 -0500

    change 050 to be valid

--- test/fixtures/issues/050.cpp
+++ test/fixtures/issues/050.cpp
@@ -1,11 +1,14 @@
-switch(test) {
-	case 1:
-	break;
-	case 2: [[fallthrough]];
-	case 3: break; // no syntax highlighting
-}
 void func1();
 [[noreturn]] void func2(/*syntax highlighting*/); // no syntax highlighting
-struct st { // syntax highlighting works now
-};
 void func3();
+
+int main() {
+    switch(test) {
+        case 1:
+        break;
+        case 2: [[fallthrough]];
+        case 3: break; // no syntax highlighting
+    }
+    struct st { // syntax highlighting works now
+    };
+}
