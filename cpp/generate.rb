5d1f8adeeb7c2d6ca9d738d751454fdb0d0afd1d
Jeff Hykin <jeff.hykin@gmail.com>
2019-05-15 17:05:32 -0500

    name the legacy_function_definition

--- cpp/generate.rb
+++ cpp/generate.rb
@@ -698,6 +698,29 @@ cpp_grammar[:qualified_type] = qualified_type = newPattern(
             ),
         includes: [ :function_call_context_c ]
         )
+    cpp_grammar[:legacy_function_definition] = PatternRange.new(
+            tag_as: "meta.function.definition.parameters",
+            # this pattern was posessive but that depends on https://github.com/jeff-hykin/cpp-textmate-grammar/issues/127
+            # integration testing says we are fin however
+            start_pattern: lookAheadToAvoid(newPattern(@cpp_tokens.that(:isOperator).or(@cpp_tokens.that(:isControlFlow))).maybe(@spaces).then(/\(/))
+                .then(
+                    match: oneOrMoreOf(identifier.or(/::/)).or(lookBehindFor(/operator/).then(@cpp_tokens.that(:canAppearAfterOperatorKeyword))),
+                    tag_as: "entity.name.function"
+                ).maybe(@spaces)
+                .then(
+                    match: /\(/,
+                    tag_as: "punctuation.section.parameters.begin.bracket.round"
+                ),
+            # no idea why this matches :
+            end_pattern: newPattern(
+                match: /\)|:/,
+                tag_as: "punctuation.section.parameters.end.bracket.round"
+            ),
+            includes: [
+                :probably_a_parameter,
+                :function_context_c,
+            ],
+        )
 #
 # Operators
 #
@@ -3007,29 +3030,7 @@ cpp_grammar[:qualified_type] = qualified_type = newPattern(
             :storage_types,
             :operators,
             :vararg_ellipses,
-            PatternRange.new(
-                tag_as: "meta.function.definition.parameters",
-                # this pattern was posessive but that depends on https://github.com/jeff-hykin/cpp-textmate-grammar/issues/127
-                # integration testing says we are fin however
-                start_pattern: lookAheadToAvoid(newPattern(@cpp_tokens.that(:isOperator).or(@cpp_tokens.that(:isControlFlow))).maybe(@spaces).then(/\(/))
-                    .then(
-                        match: oneOrMoreOf(identifier.or(/::/)).or(lookBehindFor(/operator/).then(@cpp_tokens.that(:canAppearAfterOperatorKeyword))),
-                        tag_as: "entity.name.function"
-                    ).maybe(@spaces)
-                    .then(
-                        match: /\(/,
-                        tag_as: "punctuation.section.parameters.begin.bracket.round"
-                    ),
-                # no idea why this matches :
-                end_pattern: newPattern(
-                    match: /\)|:/,
-                    tag_as: "punctuation.section.parameters.end.bracket.round"
-                ),
-                includes: [
-                    :probably_a_parameter,
-                    :function_context_c,
-                ],
-            ),
+            :legacy_function_definition,
             {
                 begin: "\\(",
                 beginCaptures: {
