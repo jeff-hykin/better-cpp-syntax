fb57fe81706f8bef059f0aa62ec35b25b4aff2a7
Jeff Hykin <jeff.hykin@gmail.com>
2019-05-28 15:07:57 -0400

    fix the generate script

--- scripts/generate.rb
+++ scripts/generate.rb
@@ -2,10 +2,10 @@ Dir.chdir __dir__ # go to this directory
 Dir.chdir ".." # go up one dir__
 
 for each_dir in Dir["source/languages/**"]
-    exit_code = Process.wait(Process.spawn("ruby", each_dir+"/generate.rb"))
-    # TODO: eventually change this to check for non-zero exit codes
-    if exit_code == -1
-        puts "Generating the syntaxes fails"
+    Process.wait(Process.spawn("ruby", each_dir+"/generate.rb"))
+    exit_code = Integer($?)
+    if exit_code != 0
+        puts "\n\nGenerating the syntax for #{File.basename(each_dir)} failed: #{exit_code}"
         exit(exit_code)
     end
 end
