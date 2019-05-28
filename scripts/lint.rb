8b79f26b79e8552e5f2a2749e932b7c07ca06037
Jeff Hykin <jeff.hykin@gmail.com>
2019-05-28 13:22:47 -0400

    add exit code check to lint

--- scripts/lint.rb
+++ scripts/lint.rb
@@ -3,5 +3,8 @@ Dir.chdir __dir__ # this file
 Dir.chdir ".."    # up one
 
 for each_file in Dir["syntaxes/**.tmLanguage.json"]
-    Process.wait(Process.spawn("node", "lint/index.js", each_file))
+    exit_code = Process.wait(Process.spawn("node", "lint/index.js", each_file))
+    if exit_code != 0
+        exit(exit_code)
+    end
 end
