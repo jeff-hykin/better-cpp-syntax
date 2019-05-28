c156a7e4be9915d934eb6c0c17d98bc22b7ee36a
Jeff Hykin <jeff.hykin@gmail.com>
2019-05-28 14:35:15 -0400

    temp change exit check

--- scripts/generate.rb
+++ scripts/generate.rb
@@ -3,7 +3,9 @@ Dir.chdir ".." # go up one dir__
 
 for each_dir in Dir["source/languages/**"]
     exit_code = Process.wait(Process.spawn("ruby", each_dir+"/generate.rb"))
-    if exit_code != 0
+    # TODO: eventually change this to check for non-zero exit codes
+    if exit_code == -1
+        puts "Generating the syntaxes fails"
         exit(exit_code)
     end
 end
