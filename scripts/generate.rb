537d6bd33ec79705d15285d6b7074154d4f455f2
Jeff Hykin <jeff.hykin@gmail.com>
2019-05-28 13:24:47 -0400

    add exit code to generate script

--- scripts/generate.rb
+++ scripts/generate.rb
@@ -2,5 +2,8 @@ Dir.chdir __dir__ # go to this directory
 Dir.chdir ".." # go up one dir__
 
 for each_dir in Dir["source/languages/**"]
-    Process.wait(Process.spawn("ruby", each_dir+"/generate.rb"))
+    exit_code = Process.wait(Process.spawn("ruby", each_dir+"/generate.rb"))
+    if exit_code != 0
+        exit(exit_code)
+    end
 end
