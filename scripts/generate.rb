a7fa4ea0ab6b5e905c52ef9729d60bd063059e7f
Matthew Fosdick <matthew.fosdick@gmail.com>
2019-05-29 15:50:32 -0700

    just exit false on failure
    some exit conditions can result in the underlying subprocess returning a
    staus code of 256. as kernel#exit only handles
    1 byte https://bugs.ruby-lang.org/issues/8083 this causes the actual
    exit code to be 0

--- scripts/generate.rb
+++ scripts/generate.rb
@@ -6,6 +6,6 @@ for each_dir in Dir["source/languages/**"]
     exit_code = Integer($?)
     if exit_code != 0
         puts "\n\nGenerating the syntax for #{File.basename(each_dir)} failed: #{exit_code}"
-        exit(exit_code)
+        exit(false)
     end
 end
