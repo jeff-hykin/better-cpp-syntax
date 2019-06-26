8122045861f83551d174de0ed6a919568e93c26c
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-26 15:35:03 -0500

    make the inline comment more efficient

--- source/shared_patterns/inline_comment.rb
+++ source/shared_patterns/inline_comment.rb
@@ -3,10 +3,41 @@ def inline_comment()
         match: /\/\*/,
         tag_as: "comment.block punctuation.definition.comment.begin",
     ).then(
-        match: /.+?/,
-        tag_as: "comment.block",
-    ).then(
-        match: /\*\//,
-        tag_as: "comment.block punctuation.definition.comment.end",
+        # this pattern is complicated because its optimized to never backtrack
+        match: newPattern(
+            tag_as: "comment.block",
+            should_fully_match: [ "thing ****/", "/* thing */", "/* thing *******/" ],
+            match: zeroOrMoreOf(
+                dont_back_track?: true,
+                match: newPattern(
+                    newPattern(
+                        oneOrMoreOf(
+                            match: /\*/,
+                            dont_back_track?: true,
+                        # any character that is not a /
+                        ).then(/[^\/]/)
+                    # any character that is not a *
+                    ).or(/[^\*]/)
+                ),
+            ).then(
+                should_fully_match: [ "*/", "*******/" ],
+                match: newPattern(
+                    oneOrMoreOf(
+                        match: /\*/,
+                        dont_back_track?: true,
+                    ).then(/\//)
+                ),
+                includes: [
+                    newPattern(
+                        match: /\*\//,
+                        tag_as: "comment.block punctuation.definition.comment.end"
+                    ),
+                    newPattern(
+                        match: /\*/,
+                        tag_as: "comment.block"
+                    ),
+                ]
+            )
+        )       
     )
 end
