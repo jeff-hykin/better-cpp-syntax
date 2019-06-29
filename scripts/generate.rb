a93ba4f0218f337afaf746b35211f45814d58ad3
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-29 18:38:52 -0500

    add ability to publish other languages

--- scripts/generate.rb
+++ scripts/generate.rb
@@ -1,8 +1,17 @@
-Dir.chdir __dir__ # go to this directory
-Dir.chdir ".." # go up one dir__
+# 
+# paths
+# 
+$path_to_root = "#{__dir__}/.."
+$path_to_languages = $path_to_root + "/source/languages"
+def pathToGenerator(language_extension_name)
+    return $path_to_languages+"/#{language_extension_name}/generate.rb"
+end
 
+# 
+# Helpers
+# 
 def build(language_extension_name)
-    Process.wait(Process.spawn("ruby", language_extension_name+"/generate.rb"))
+    Process.wait(Process.spawn("ruby", pathToGenerator(language_extension_name)))
     exit_code = Integer($?)
     if exit_code != 0
         puts "\n\nGenerating the syntax for '#{language_extension_name}' failed: #{exit_code}"
@@ -10,10 +19,13 @@ def build(language_extension_name)
     end
 end
 
+# 
+# Process CommandLine input
+# 
 # if no args then build all of them
 if ARGV[0] == nil
-    for each_dir in Dir["source/languages/**"]
-        build(language_extension_name)
+    for each_dir in Dir[$path_to_languages+"/**"]
+        build(File.basename(each_dir))
     end
 else
     build(ARGV[0])
