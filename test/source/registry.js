3bebfafa60fd2a31a3cecbb8fd65b99cfb804b01
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-29 19:20:50 -0500

    let registry work for any lang

--- test/source/registry.js
+++ test/source/registry.js
@@ -1,46 +1,21 @@
 const path = require("path");
 const fs = require("fs");
+const glob = require("glob");
 const vsctm = require("vscode-textmate");
 const rewriteGrammar = require("./report/rewrite_grammar");
 
 const testDir = path.dirname(__dirname);
 
+
 function getRegistry(getOnigLib) {
     return new vsctm.Registry({
         loadGrammar: scopeName => {
-            let grammarPath = "";
-            switch (scopeName) {
-                case "source.cpp":
-                    grammarPath = path.join(
-                        testDir,
-                        "../syntaxes",
-                        "cpp.tmLanguage.json"
-                    );
-                    break;
-                case "source.c":
-                    grammarPath = path.join(
-                        testDir,
-                        "../syntaxes",
-                        "c.tmLanguage.json"
-                    );
-                    break;
-                case "source.objcpp":
-                    grammarPath = path.join(
-                        testDir,
-                        "../syntaxes",
-                        "objcpp.tmLanguage.json"
-                    );
-                    break;
-                case "source.objc":
-                    grammarPath = path.join(
-                        testDir,
-                        "../syntaxes",
-                        "objc.tmLanguage.json"
-                    );
-                    break;
-                default:
-                    console.error("requested non c/c++/objc/objc++ grammar");
-                    return Promise.resolve({});
+            let extension = scopeName.replace(/source\./, "");
+            let grammarPath = `${testDir}/../syntaxes/${extension}.tmLanguage.json`;
+            // check if the syntax exists
+            if (!fs.existsSync(grammarPath)) {
+                console.error("requested grammar outside of this repository");
+                return Promise.resolve({});
             }
             return Promise.resolve(
                 rewriteGrammar(
