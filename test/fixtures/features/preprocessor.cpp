1bb1877b8c1b71ee8a052330bba82b7bb0099e72
Jeff Hykin <jeff.hykin@gmail.com>
2019-07-08 12:04:12 -0500

    add preprocessor checks

---
+++ test/fixtures/features/preprocessor.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "relative_stuff"
+
+
+#define a thing {}
+#define a(arg1, arg2) arg1 arg2
+#define a(arg1, arg2) #macro_variable ##macro_variable
+
+#define thing class Stuff
+
+#define thing struct Stuff 
+#define thing struct Stuff \
+    {
+
+
+#pragma once
+
+#include <type_traits>
+
+#define IsPointDef(...) \
+    template<> \
+    struct IsPoint<__VA_ARGS__> \
+        {\
+        static const bool isPoint = true;\
+                }
+
+#define ArrayBasedPointDef(T) \
+    IsPointDef(T); \
+    template<> \
+    struct IsArrayBasedPoint<T>:public std::true_type \
+        {
+            
