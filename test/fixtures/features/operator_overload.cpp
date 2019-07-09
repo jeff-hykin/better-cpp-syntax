7d732e9cc4e9ba1ddfeaf3e591b9feb45acc9360
Jeff Hykin <jeff.hykin@gmail.com>
2019-07-08 22:59:11 -0500

    _

--- test/fixtures/features/operator_overload.cpp
+++ test/fixtures/features/operator_overload.cpp
@@ -1,17 +1,17 @@
-// Item  operator+( const string&  base        , const int&    repetitions ) {};
-// Item  operator-( const int&     the_input   , const Item&   input_item  ) {};
-// Item  operator/( const Item&    input_item  , const int&    the_input   ) {};
-// Item  operator^( const Item&    input_item  , const int&    the_input   ) {};
-// inline ostream& operator,(ostream& o, ostream& (*manip_fun)(ostream&)) {};
+Item  operator+( const string&  base        , const int&    repetitions ) {};
+Item  operator-( const int&     the_input   , const Item&   input_item  ) {};
+Item  operator/( const Item&    input_item  , const int&    the_input   ) {};
+Item  operator^( const Item&    input_item  , const int&    the_input   ) {};
+inline ostream& operator,(ostream& o, ostream& (*manip_fun)(ostream&)) {};
 
 // templated type conversion
-// operator std::basic_string_view<CharT, Traits>() const noexcept {}
+operator std::basic_string_view<CharT, Traits>() const noexcept {}
 
 // resolution type conversion
 operator std::basic_string_view() const noexcept {}
 
 // implicit conversion
-operator string() const  {}
+operator std::thing::string() const  {}
 operator double() const  {} 
 
 
