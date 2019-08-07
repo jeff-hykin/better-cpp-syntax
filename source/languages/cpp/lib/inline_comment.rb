89d7c23dce4ff954568a65e60549bae8e35ad3ea
Jeff Hykin <jeffhykin@jeffs-imac.lan>
2019-08-07 16:59:57 -0500

    switch to Pattern.new

--- source/languages/cpp/lib/inline_comment.rb
+++ source/languages/cpp/lib/inline_comment.rb
@@ -2,18 +2,18 @@ require_relative '../../../../directory'
 require_relative PathFor[:textmate_tools]
 
 Grammar.export(insert_namespace_infront_of_new_grammar_repos: true, insert_namespace_infront_of_all_included_repos: false) do |grammar, namespace|
-    newPattern(
+    Pattern.new(
         match: /\/\*/,
         tag_as: "comment.block punctuation.definition.comment.begin",
     ).then(
         # this pattern is complicated because its optimized to never backtrack
-        match: newPattern(
+        match: Pattern.new(
             tag_as: "comment.block",
             should_fully_match: [ "thing ****/", "/* thing */", "/* thing *******/" ],
             match: zeroOrMoreOf(
                 dont_back_track?: true,
-                match: newPattern(
-                    newPattern(
+                match: Pattern.new(
+                    Pattern.new(
                         /[^\*]/
                     ).or(
                         oneOrMoreOf(
@@ -25,18 +25,18 @@ Grammar.export(insert_namespace_infront_of_new_grammar_repos: true, insert_names
                 ),
             ).then(
                 should_fully_match: [ "*/", "*******/" ],
-                match: newPattern(
+                match: Pattern.new(
                     oneOrMoreOf(
                         match: /\*/,
                         dont_back_track?: true,
                     ).then(/\//)
                 ),
                 includes: [
-                    newPattern(
+                    Pattern.new(
                         match: /\*\//,
                         tag_as: "comment.block punctuation.definition.comment.end"
                     ),
-                    newPattern(
+                    Pattern.new(
                         match: /\*/,
                         tag_as: "comment.block"
                     ),
