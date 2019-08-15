03afa3ed15435e125183b1242a59221fdcbaabfb
Matthew Fosdick <matthew.fosdick@gmail.com>
2019-08-15 11:06:31 -0700

    Add test case

---
+++ test/fixtures/issues/353.cpp
@@ -0,0 +1,7 @@
+/**
+ * @brief performs a "foo" action
+ * @param a the action to preform
+ * @retval ERROR_SUCCESS action performed successfully
+ * @retval EPERM permission error
+ */
+int foo(int a);
