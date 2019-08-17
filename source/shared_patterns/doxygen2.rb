69122e4a7b48458fdbdeeed2d07569fd24a289ad
Matthew Fosdick <matthew.fosdick@gmail.com>
2019-08-17 16:43:26 -0700

    More work on comprehensive doxygen grammar

--- source/shared_patterns/doxygen2.rb
+++ source/shared_patterns/doxygen2.rb
@@ -86,6 +86,22 @@ paragraph_commands = [
 
 paragraph_range_pattern_end = lookAheadFor(/[\\@](?:#{paragraph_commands.join("|")})/.or(/\*\//)).or(/\s++/.then(@end_of_line))
 
+def generate_line_command_pattern(command_pattern, line_includes)
+    Pattern.new(
+        match: lookBehindFor(/[\s*!\/]/).then(/[\\@]/).then(command_pattern),
+        tag_as: "storage.type.class.doxygen",
+    ).then(@spaces).then(
+        match: /.+/,
+        includes: [
+            PatternRange.new(
+                start_pattern: lookAheadFor(/./),
+                end_pattern: /$/,
+                includes: line_includes
+            )
+        ],
+    )
+end
+
 def generate_paragraph_command_pattern(command_pattern, first_line_includes, body_includes)
     PatternRange.new(
         tag_as: "meta.paragraph"
@@ -95,23 +111,19 @@ def generate_paragraph_command_pattern(command_pattern, first_line_includes, bod
             ).then(
                 match: /.+/,
                 includes: [
-                    *first_line_includes,
-                    *body_includes,
+                    PatternRange.new(
+                        start_pattern: lookAheadFor(/./),
+                        end_pattern: /$/,
+                        includes: includes: [
+                            *first_line_includes,
+                            *body_includes,
+                        ],
+                    )
                 ],
             ),
         end_pattern: paragraph_range_pattern_end,
         includes: body_includes,
 
-    )
-end
-
-def generate_word_command_pattern(command_pattern, word_tag)
-    Pattern.new(
-        match: lookBehindFor(/[\s*!\/]/).then(/[\\@]/).then(command_pattern),
-        tag_as: "storage.type.class.doxygen",
-    ).then(@spaces).then(
-        match: /\S+/,
-        tag_as: word_tag,
     )
 end
 
@@ -122,9 +134,23 @@ Grammar.export(insert_namespace_infront_of_new_grammar_repos: true, insert_names
         ).then(/\b/).maybe(/\{[^}]*\}/),
         tag_as: "storage.type.class.doxygen"
     )
+    # word commands
     grammar[:italics] = generate_word_command_pattern(/(?:a|em|e)/, "markup.italic.doxygen")
     grammar[:bold] = generate_word_command_pattern(/(?:b)/, "markup.bold.doxygen")
-    grammar[:code] = generate_word_command_pattern(/(?:c|p)/, "markup.inline.raw.string")
+    grammar[:code] = generate_word_command_pattern(/(?:c|p)/, "markup.inline.raw.string.doxygen")
+    grammar[:type_word] = generate_word_command_pattern(/(?:c|p)/, "entity.name.type.doxygen")
+    grammar[:nontype_word] = generate_word_command_pattern(/(?:def|p)/, "variable.other.doxygen")
     grammar[:reference] = generate_word_command_pattern(/(?:anchor|cite|copybrief|copydetails|copydoc|)/, "constant.other.reference.doxygen")
-    grammar[:default_paragraph] = generate_word_command_pattern(/(?:arg|attention|authors?|brief|bug|copyright|date)/,[],[:standalone])
+    # line commands
+    grammar[:type_line] = generate_word_command_pattern(/(?:class)/, [
+        Pattern.new(
+            match: Pattern.new(/\G\s++/).then(
+                match: /\S+/,
+                tag_as: "entity.name.type.doxygen"
+            )
+        )
+    ])
+    # paragraph commands
+    grammar[:default_paragraph] = generate_word_command_pattern(/(?:arg|attention|authors?|brief|bug|copyright|date)/, [],[:standalone])
+    # block commands
 end
