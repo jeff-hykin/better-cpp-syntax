e5756f5172bcf99831eb413ab49234c0616c4db1
Matthew Fosdick <matthew.fosdick@gmail.com>
2019-08-19 17:36:33 -0700

    Statefully remove unneeded patterns

--- scripts/duplicateForEmbedding.js
+++ scripts/duplicateForEmbedding.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const _ = require("lodash");
 
 duplicateForEmbedding("cpp", "latex", "\\\\end\\{minted\\}");
 // add other languages here
@@ -13,19 +14,61 @@ function duplicateForEmbedding(language, scope, early_bailout_pattern) {
     );
     originalScopeName = grammar["scopeName"];
     grammar["scopeName"] = grammar["scopeName"] + ".embedded." + scope;
-    rewriteRules(originalScopeName, grammar, early_bailout_pattern);
+    const deleteableRules = selectRulesForDeletion(grammar);
+    console.log(deleteableRules);
+    rewriteRules(
+        originalScopeName,
+        deleteableRules,
+        grammar,
+        early_bailout_pattern
+    );
     fs.writeFileSync(
         `${__dirname}/../syntaxes/${language}.embedded.${scope}.tmLanguage.json`,
         JSON.stringify(grammar)
     );
 }
 
-function rewriteRules(
-    originalscopeName,
-    rule,
-    newEnding,
-    repositoryName = undefined
-) {
+function selectRulesForDeletion(rule, repositoryName = undefined) {
+    if (rule.match) {
+        if (repositoryName) {
+            return [repositoryName];
+        }
+        return [];
+    }
+    if (rule.patterns) {
+        if (Object.keys(rule).length == 1) {
+            const deleteable = rule.patterns.reduce((array, r) => {
+                if (!array) return array;
+                const deleteable = selectRulesForDeletion(r);
+                if (!deleteable) return false;
+                return array.concat(deleteable);
+            }, []);
+            if (deleteable) {
+                return (repositoryName && [repositoryName]) || [];
+            }
+        }
+    }
+    if (rule.repository) {
+        return Object.keys(rule.repository).reduce((array, key) => {
+            const deleteable = selectRulesForDeletion(
+                rule.repository[key],
+                key
+            );
+            if (deleteable) {
+                array = array.concat(deleteable);
+            }
+            return array;
+        }, []);
+    }
+    return false;
+}
+
+function rewriteRules(originalscopeName, deleteableRules, rule, newEnding) {
+    if (rule.include) {
+        if (_.includes(deleteableRules, rule.include.slice(1))) {
+            rule.include = `${originalscopeName}${rule.include}`;
+        }
+    }
     if (rule.end) {
         rule.end = `${rule.end}|(?=${newEnding})`;
     }
@@ -33,70 +76,60 @@ function rewriteRules(
         rule.while = `${rule.while}|(?:^\\s+(?!${newEnding}))`;
     }
     if (rule.patterns) {
-        if (Object.keys(rule).length == 1) {
-            // If this is a pattern only rule, attempt to rewrite
-            const rewrite = rule.patterns.reduce((rewrite, r) => {
-                return rewrite && rewriteRules(originalscopeName, r, newEnding);
-            });
-            if (rewrite) {
-                if (!repositoryName || rule.patterns.length == 1) {
-                    return true;
-                }
-                rule.patterns = [
-                    { include: `${originalscopeName}#${repositoryName}` }
-                ];
-            }
-        } else {
-            rule.patterns.map(r =>
-                rewriteRules(originalscopeName, r, newEnding)
-            );
-        }
+        rule.patterns.map(r =>
+            rewriteRules(originalscopeName, deleteableRules, r, newEnding)
+        );
     }
     // rule.repository is apparently allowed, at least in vscode-textmate, its not documented however
     if (rule.repository) {
+        rule.repository = _.omit(rule.repository, deleteableRules);
         Object.keys(rule.repository).map(key =>
             rewriteRules(
                 originalscopeName,
+                deleteableRules,
                 rule.repository[key],
-                newEnding,
-                key
+                newEnding
             )
         );
     }
     if (rule.captures) {
         Object.keys(rule.captures).map(key => {
-            rewriteRules(originalscopeName, rule.captures[key], newEnding);
+            rewriteRules(
+                originalscopeName,
+                deleteableRules,
+                rule.captures[key],
+                newEnding
+            );
         });
     }
     if (rule.beginCaptures) {
         Object.keys(rule.beginCaptures).map(key => {
-            rewriteRules(originalscopeName, rule.beginCaptures[key], newEnding);
+            rewriteRules(
+                originalscopeName,
+                deleteableRules,
+                rule.beginCaptures[key],
+                newEnding
+            );
         });
     }
     if (rule.endCaptures) {
         Object.keys(rule.endCaptures).map(key => {
-            rewriteRules(originalscopeName, rule.endCaptures[key], newEnding);
+            rewriteRules(
+                originalscopeName,
+                deleteableRules,
+                rule.endCaptures[key],
+                newEnding
+            );
         });
     }
     if (rule.whileCaptures) {
         Object.keys(rule.whileCaptures).map(key => {
-            rewriteRules(originalscopeName, rule.whileCaptures[key], newEnding);
+            rewriteRules(
+                originalscopeName,
+                deleteableRules,
+                rule.whileCaptures[key],
+                newEnding
+            );
         });
     }
-    if (rule.match) {
-        // match rules cannot effect the ability to bailout as
-        // match rules do not have the ability to overshoot
-        // replace them with an include to the base grammar
-        if (!repositoryName) {
-            // if there is not currently a valid rewrite target, signal that this could
-            // have been rewritten
-            return true;
-        }
-        rule.patterns = [{ include: `${originalscopeName}#${repositoryName}` }];
-        // cleanup rule, assignment doesn't work
-        delete rule["match"];
-        delete rule["name"];
-        delete rule["captures"];
-    }
-    return false;
 }
