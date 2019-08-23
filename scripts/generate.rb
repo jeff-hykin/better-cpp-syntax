8d44048722a95555f6fe4d1486f33cc2df6d3e07
Matthew Fosdick <matthew.fosdick@gmail.com>
2019-08-22 20:16:29 -0700

    Have generate use mtime to decide what grammars to generate

--- scripts/generate.rb
+++ scripts/generate.rb
@@ -1,8 +1,48 @@
 require_relative '../directory.rb'
 # 
 # Helpers
-# 
+#
+
+# grabs uncommited files exactly once
+$uncommited_files = nil
+def get_uncommited
+    if $uncommited_files == nil
+        git_status = `git status --porcelain`
+        $uncommited_files = git_status.split("\n").filter {|file| /^.M/ =~ file}.map do |file|
+            /M (.+)/ =~ file
+            File.join(PathFor[:root], $1)
+        end
+    end
+    $uncommited_files
+end
+
+# checks with git to get correct mtime
+def mtime(file)
+    return File.mtime(file) if get_uncommited().include? file
+    time = `git log -1 --format="%ad" --date=unix #{file}`
+    Time.at(time.to_i)
+end
+
+def should_build(language_extension_name)
+    if not File.exists?(PathFor[:jsonSyntax][language_extension_name])
+        return true
+    end
+    compare_time = mtime(PathFor[:jsonSyntax][language_extension_name])
+    Dir.glob("#{PathFor[:source]}/*.rb") do |file|
+        return true if mtime(file) > compare_time
+    end
+    Dir.glob("#{PathFor[:source]}/shared_patterns/*.rb") do |file|
+        return true if mtime(file) > compare_time
+    end
+    Dir.glob("#{PathFor[:language][language_extension_name]}/**/*.rb") do |file|
+        return true if mtime(file) > compare_time
+    end
+    return false
+end
+
+
 def build(language_extension_name)
+    return if not should_build language_extension_name
     Process.wait(Process.spawn("ruby", PathFor[:generator][language_extension_name]))
     exit_code = Integer($?)
     if exit_code != 0
