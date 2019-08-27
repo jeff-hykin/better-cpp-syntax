ec0a1ae5c7a87f27e663ee9a5f68155596f1100c
Matthew Fosdick <matthew.fosdick@gmail.com>
2019-08-27 13:08:06 -0700

    more grammar skeleton

--- source/rewrite/grammar.rb
+++ source/rewrite/grammar.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: true
 
 require 'digest/sha1'
+require 'json'
 
 require_relative 'pattern'
 require_relative 'pattern_range'
 require_relative 'sub_patterns'
 
 class Grammar
+    def self.new_exportable_grammar
+        ExportableGrammar.new
+    end
+
+    def self.import(path)
+        import_grammar = JSON.parse File.read(path)
+        grammar = Grammar.new(
+            import_grammar["name"],
+            import_grammar["scopeName"],
+        )
+        # import "patterns" into @repository[:$initial_context]
+        # import the rest of the repository
+        grammar
+    end
     def initialize(name, scope_name)
         @name = name
         @scope_name = scope_name
@@ -20,7 +35,7 @@ class Grammar
     def []=(key, value)
         raise "Use symbols not strings" unless key.is_a? Symbol
 
-        if key.to_s.start_with?("$") && !(["$initial_context", "$base", "$self"].include? key)
+        if key.to_s.start_with?("$") && !(["$initial_context"].include? key)
             puts "#{key} is not a valid repository name"
             puts "repository names starting with $ are reserved"
             raise "See above error"
@@ -45,10 +60,6 @@ class Grammar
         @repository[key] = value
         @repository[key]
     end
-
-    def self.new_exportable_grammar
-        ExportableGrammar.new
-    end
 end
 
 class ExportableGrammar < Grammar
@@ -61,5 +72,20 @@ class ExportableGrammar < Grammar
         # Don't do that
         @seed = Digest::MD5.hexdigest(caller_locations(3, 1).first.path).slice(0, 10)
         puts @seed
+    end
+
+    def []=(key, value)
+        if key.to_s == "$initial_context"
+            puts "ExportGrammar cannot store to $initial_context"
+            raise "See error above"
+        end
+        super(key, value)
+    end
+
+    def export
+        # convert all repository keys to a prefixed version unless in exports
+        # prefix all include symbols unless in external_repos
+        # ensure the grammar does not refer to a symbol not in repository or external_repos
+        # ensure the grammar has all keys named in exports
     end
 end
