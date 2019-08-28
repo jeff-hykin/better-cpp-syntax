af464fc6d43228e596a0cbc21e0e7891e57ea663
Matthew Fosdick <matthew.fosdick@gmail.com>
2019-08-28 11:07:11 -0700

    Finish initial import and export

--- source/rewrite/grammar.rb
+++ source/rewrite/grammar.rb
@@ -7,21 +7,33 @@ require_relative 'pattern'
 require_relative 'pattern_range'
 require_relative 'sub_patterns'
 
+def top_level_binding
+    binding
+end
+
 class Grammar
+    attr_accessor repository
     def self.new_exportable_grammar
         ExportableGrammar.new
     end
 
-    def self.import(path)
+    def self.fromTmLanguage(path)
         import_grammar = JSON.parse File.read(path)
-        grammar = Grammar.new(
+
+        grammar = ImportGrammar.new(
             import_grammar["name"],
             import_grammar["scopeName"],
         )
         # import "patterns" into @repository[:$initial_context]
+        grammar.repository[:$initial_context] = import_grammar["patterns"]
         # import the rest of the repository
+        import_grammar["repository"].each do |key, value|
+            # repository keys are kept as a hash
+            grammar.repository[key[1..-1].to_sym] = value
+        end
         grammar
     end
+
     def initialize(name, scope_name)
         @name = name
         @scope_name = scope_name
@@ -60,6 +72,27 @@ class Grammar
         @repository[key] = value
         @repository[key]
     end
+
+    def import(path_or_export)
+        export = path_or_export
+        unless path_or_export.is_a? ExportableGrammar
+            path = path_or_export
+            file = File.read path_or_export
+            export = eval(file, top_level_binding, path, 0) # rubocop:disable Security/Eval
+
+            unless export.is_a? ExportableGrammar
+                puts "The file #{file} returned an object that was not obtained from"
+                puts "Grammar::new_exportable_grammar, this is not allowed"
+                puts "class: #{exports.class}"
+                raise "See above error"
+            end
+        end
+
+        # import the repository
+        @repository = @repository.merge export.repository do |_key, old_val, new_val|
+            [old_val, new_val].flatten
+        end
+    end
 end
 
 class ExportableGrammar < Grammar
@@ -87,5 +120,19 @@ class ExportableGrammar < Grammar
         # prefix all include symbols unless in external_repos
         # ensure the grammar does not refer to a symbol not in repository or external_repos
         # ensure the grammar has all keys named in exports
+    end
+end
+
+class ImportGrammar < Grammar
+    def initialize(*args)
+        super(args)
+    end
+
+    def [](key)
+        unless @repository[key].is_a? Pattern
+            raise "#{key} is a not a Pattern and cannot be referenced"
+        end
+
+        @repository[key]
     end
 end
