4be48cf361445fe1be33796ac3a83cc773cf4390
Matthew Fosdick <matthew.fosdick@gmail.com>
2020-06-20 12:51:19 -0700

    prevent placeholder loop

--- gem/lib/textmate_grammar/transforms/resolve_placeholders.rb
+++ gem/lib/textmate_grammar/transforms/resolve_placeholders.rb
@@ -8,14 +8,18 @@ class ResolvePlaceholders < GrammarTransform
         return pattern unless pattern.is_a? PatternBase
         pattern_copy = pattern.__deep_clone__
         pattern_copy.map!(true) do |each_pattern_like|
-            
+
             arguments = each_pattern_like.arguments
             repository = options[:repository]
-            
+
             if each_pattern_like.is_a?(PlaceholderPattern)
-                
+
                 unless repository[arguments[:placeholder]].is_a? PatternBase
                     raise ":#{arguments[:placeholder]} is not a pattern and cannot be substituted"
+                end
+
+                if options[:grammar].pattern_processing == repository[arguments[:placeholder]]
+                    raise ":#{arguments[:placeholder]} is being processed and cannot be substituted"
                 end
 
                 each_pattern_like.arguments[:match] = repository[arguments[:placeholder]].__deep_clone__
@@ -37,16 +41,16 @@ class ResolvePlaceholders < GrammarTransform
                             bad_matches << each_value
                         end
                     end
-                    
+
                     raise <<~HEREDOC
-                        
-                        
+
+
                         When using the selector: #{arguments[:selector]}
                         After looking at all patterns stored in the grammar repository
                         (e.g. grammar[:something] = Pattern.new())
                         None of their adjective lists matched the collector filter
                         #{<<~HEREDOC if bad_matches.length > 0
-                            
+
                             It appears the selector DOES match some patterns
                             But those patterns are not in the grammar repo
                             If you want to match those patterns, add them to the grammar repo
@@ -57,7 +61,7 @@ class ResolvePlaceholders < GrammarTransform
                         }
                     HEREDOC
                 end
-                
+
                 # lookAhead/behind optimization
                 # if all of them are keyword patterns then extract out the keywords and only put lookAhead/lookBehinds at the ends
                 if qualifying_patterns.all? { |each| each.arguments[:keyword] != nil }
@@ -76,7 +80,7 @@ class ResolvePlaceholders < GrammarTransform
         pattern_copy.freeze
     end
 end
-                        
+
 # resolving placeholders has no dependencies and makes analyzing patterns much nicer
 # so it happens fairly early
 Grammar.register_transform(ResolvePlaceholders.new, 0)
