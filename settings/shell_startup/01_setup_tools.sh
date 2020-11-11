bd46e75a911ade16b928a3b6f5a6a4bd9efb010d
Jeff Hykin <jeff.hykin@gmail.com>
2020-11-11 11:02:18 -0600

    fix/improve the add_to_gitignore

--- settings/shell_startup/01_setup_tools.sh
+++ settings/shell_startup/01_setup_tools.sh
@@ -5,7 +5,7 @@ escape_grep_regex() {
 function add_to_gitignore {
     touch .gitignore
     escaped_name="$(escape_grep_regex $1)"
-    grep "$escaped_name$" .gitignore &>/dev/null || echo "
+    grep -E -- "$escaped_name$" .gitignore &>/dev/null || echo "
 
 # this next line was auto-added, and may be very important (passwords/auth etc)
 # comment it out if dont want it to be ignored (and you know what you're doing)
