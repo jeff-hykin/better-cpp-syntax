5ec80a65634b29fd089924125acee4b336b1d695
Jeff Hykin <jeff.hykin@gmail.com>
2020-11-11 09:55:49 -0600

    improve gitignore tool

--- settings/shell_startup/01_setup_tools.sh
+++ settings/shell_startup/01_setup_tools.sh
@@ -7,6 +7,7 @@ function add_to_gitignore {
     escaped_name="$(escape_grep_regex $1)"
     grep "$escaped_name$" .gitignore &>/dev/null || echo "
 
-# this next line was auto-added, comment it out if dont want it to be ignored 
+# this next line was auto-added, and may be very important (passwords/auth etc)
+# comment it out if dont want it to be ignored (and you know what you're doing)
 $1" >> .gitignore
 }
