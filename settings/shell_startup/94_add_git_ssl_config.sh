e3815c6798c997fc91c758f4761158f9c101a7ec
Jeff Hykin <jeff.hykin@gmail.com>
2020-11-15 07:53:19 -0600

    fixup pip and git

---
+++ settings/shell_startup/94_add_git_ssl_config.sh
@@ -0,0 +1,4 @@
+# Probably should find a better solution that disabling verification
+# Here's the relevent webpage explaining the issue: 
+# https://confluence.atlassian.com/bitbucketserverkb/ssl-certificate-problem-unable-to-get-local-issuer-certificate-816521128.html
+git config --global http.sslVerify false
