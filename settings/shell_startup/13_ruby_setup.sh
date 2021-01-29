d867a71647f1ff42c7c8b1cdd8987d84f819a39f
Jeff Hykin <jeff.hykin@gmail.com>
2021-01-29 01:33:28 +0000

    get ruby gems working

---
+++ settings/shell_startup/13_ruby_setup.sh
@@ -0,0 +1,3 @@
+export GEM_HOME="$HOME/gems.nosync/"
+mkdir "$GEM_HOME" &>/dev/null
+./settings/commands/.check_ruby_gems
--- shell.nix
+++ shell.nix
@@ -44,7 +44,7 @@ let
             })
         ) packageJson.nix.packages;
     };
-    
+  
     # TODO: add support for the info.json to have nested packages
     nestedPackages = [
         # 
@@ -81,20 +81,51 @@ let
         definitions.mainPackages.unixtools.xxd
     ];
     
+    majorCustomDependencies = rec {
+        ruby = [
+            # 
+            # ruby
+            # 
+            definitions.mainPackages.ruby.devEnv
+            definitions.mainPackages.git
+            definitions.mainPackages.sqlite
+            definitions.mainPackages.libpcap
+            definitions.mainPackages.postgresql
+            definitions.mainPackages.libxml2
+            definitions.mainPackages.libxslt
+            definitions.mainPackages.pkg-config
+            definitions.mainPackages.bundix
+            definitions.mainPackages.gnumake
+            (definitions.mainPackages.bundlerEnv {
+                name = "gems";
+                ruby = definitions.mainPackages.ruby;
+                gemdir = ./settings/requirements;
+            })
+        ];
+        python = [
+            definitions.mainPackages.python37
+            definitions.mainPackages.python37Packages.setuptools
+            definitions.mainPackages.python37Packages.pip
+            definitions.mainPackages.python37Packages.virtualenv
+        ];
+    };
+    
+    subDepedencies = [] ++ majorCustomDependencies.ruby ++ [];
+    
     # TODO: add support for the info.json to have OS-specific packages (if statement inside package inclusion)
-    packagesForMacOnly = [] ++ definitions.mainPackages.lib.optionals (definitions.mainPackages.stdenv.isDarwin) [
-        # python and venv
-        definitions.mainPackages.python37
-        definitions.mainPackages.python37Packages.setuptools
-        definitions.mainPackages.python37Packages.pip
-        definitions.mainPackages.python37Packages.virtualenv
-    ];
+    packagesForMacOnly = [] ++ definitions.mainPackages.lib.optionals (definitions.mainPackages.stdenv.isDarwin) (
+        majorCustomDependencies.python ++ [
+        
+    ]);
+    
+    
+    
 # using those definitions
 in
     # create a shell
     definitions.mainPackages.mkShell {
         # inside that shell, make sure to use these packages
-        buildInputs = nestedPackages ++ packagesForMacOnly ++ builtins.map (each: each.source) definitions.packagesWithSources;
+        buildInputs = subDepedencies ++ nestedPackages ++ packagesForMacOnly ++ builtins.map (each: each.source) definitions.packagesWithSources;
         
         # run some bash code before starting up the shell
         shellHook = ''
