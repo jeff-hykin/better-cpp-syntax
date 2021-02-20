e577fc491893b7047d307944ad3e290f3d5b907c
Jeff Hykin <jeff.hykin@gmail.com>
2021-02-19 20:48:13 -0600

    add gemset

---
+++ settings/requirements/gemset.nix
@@ -0,0 +1,21 @@
+{
+  deep_clone = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["http://rubygems.org"];
+      sha256 = "003iqvmxxcm7p6qr2aafi1p5djm6ycvwzrjk6shc9wvybkvbckmz";
+      type = "gem";
+    };
+    version = "0.0.1";
+  };
+  textmate_grammar = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      path = ../../gem;
+      type = "path";
+    };
+    version = "0.0.0";
+  };
+}
