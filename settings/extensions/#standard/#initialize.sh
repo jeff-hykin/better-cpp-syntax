f46757c70b6690bb90f1b5f88126d45805212774
Jeff Hykin <jeff.hykin@gmail.com>
2021-08-26 10:33:17 -0500

    add link_extension_file__to__

--- settings/extensions/#standard/#initialize.sh
+++ settings/extensions/#standard/#initialize.sh
@@ -1,3 +1,43 @@
+#!/usr/bin/env bash
+
+__temp_var__this_extension="#standard"
+
+# create extension-linker as a helper
+link_extension_file__to__() {
+    local_file="$1"
+    target_file="$2"
+    
+    # check for absolute path, if not absolute make it relative to project/settings
+    case "$target_file" in
+        /*) __temp_var__is_absolute_path="true" ;;
+        *) : ;;
+    esac
+    if ! [ "$__temp_var__is_absolute_path" = "true" ]
+    then
+        __temp_var__target_full_path="$PROJECTR_FOLDER/settings/$target_file"
+    else
+        __temp_var__target_full_path="$target_file"
+    fi
+    unset __temp_var__is_absolute_path
+    
+    # remove existing things in the way
+    rm -f "$__temp_var__target_full_path" 2>/dev/null
+    rm -rf "$__temp_var__target_full_path" 2>/dev/null
+    # make sure parent folder exists
+    mkdir -p "$(dirname "$__temp_var__target_full_path")"
+    if [ -n "$$__temp_var__this_extension" ]
+    then
+        # link the file (relative link, which it what makes it complicated)
+        final_path="$(realpath "$PROJECTR_FOLDER/settings/" --relative-to="$(dirname "$__temp_var__target_full_path")" --canonicalize-missing)/extensions/$__temp_var__this_extension/$local_file"
+        ln -s "$(realpath "$PROJECTR_FOLDER/settings/" --relative-to="$(dirname "$__temp_var__target_full_path")" --canonicalize-missing)/extensions/$__temp_var__this_extension/$local_file" "$__temp_var__target_full_path"
+    else
+        echo 'An extension forgot to set $__temp_var__this_extension, and therefore I dont know which extension is doing it. (its calling link_extension_file__to__ '"'$1' '$2' )"
+    fi
+    
+    unset __temp_var__target_folder
+    unset local_file
+    unset automatic_setup_name
+}
 
 # 
 # connect when_purging
@@ -6,7 +46,17 @@ mkdir -p "$PROJECTR_FOLDER/settings/.cache/"
 mkdir -p "$PROJECTR_FOLDER/settings/when_purging/"
 mkdir -p "$PROJECTR_FOLDER/settings/when_cleaning/"
 
-ln "the_file_that_exist" "the_file_that_doesnt"
+# 
+# connect automatic_setup/manual_setup
+# 
+link_extension_file__to__ "tools/projectr/ensure_all_commands_executable" "automatic_setup/08100#ensure_all_commands_executable.sh"
+link_extension_file__to__ "tools/projectr/ensure_all_commands_executable" "manual_setup/08100#ensure_all_commands_executable.sh"
+
+# 
+# connect commands
+# 
+link_extension_file__to__ "tools/projectr" "$PROJECTR_COMMANDS_FOLDER/tools/projectr"
+link_extension_file__to__ "tools/string"   "$PROJECTR_COMMANDS_FOLDER/tools/string"
 
 # 
 # flush broken symlinks (for when extensions are changed/removed)
@@ -45,3 +95,9 @@ for_each_item_in="$PROJECTR_FOLDER/requirements"; [ -z "$__NESTED_WHILE_COUNTER"
 do
     rm -f "$each" 2>/dev/null
 done < "$__temp_var__temp_folder/pipe_for_while_$__NESTED_WHILE_COUNTER";__NESTED_WHILE_COUNTER="$((__NESTED_WHILE_COUNTER - 1))"
+
+
+
+
+
+unset __temp_var__this_extension
