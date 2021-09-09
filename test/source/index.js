3626f8fe15c982d7bf6106f83c893728ad188b94
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-09 12:30:56 -0500

    remove old test folder

--- test/source/index.js
+++
@@ -1,4 +0,0 @@
-const argv = require("./arguments");
-argv.parseArgs();
-
-// purposely blank, the command dispatch is done with yargs
--- test/source/symbols.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    performanceForEachFixture: Symbol("performanceForEachFixture"),
-    currentActiveFixture: Symbol("currentActiveFixture"),
-}
