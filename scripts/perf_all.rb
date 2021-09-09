dfd7b8733a436093acdc57f99987e78ee1bc734a
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-09 12:32:05 -0500

    remove old stuff

--- scripts/perf_all.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../paths.rb'
-
-for each in Dir[PathFor[:fixtures]+"/**/*"]
-    if File.file?(each)
-        puts "#{File.basename(each)}"
-        `node #{PathFor[:report]} perf2 "#{each}"`
-    end
-end
