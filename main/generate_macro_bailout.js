7e75905dcf41c0831fa8f23fdbb8d7e08f2b3842
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-12 13:50:05 -0500

    remove file types from embedded grammar

--- main/generate_macro_bailout.js
+++ main/generate_macro_bailout.js
@@ -1,10 +1,17 @@
-let duplicateForEmbedding = require("textmate-bailout");
-let { argv } = require("process");
+const duplicateForEmbedding = require("textmate-bailout")
+const fs = require("fs")
+const { argv } = require("process")
 
+const newFileLocation = argv[3]
 duplicateForEmbedding({
     grammarFilePath: argv[2],
     appendScope: "macro",
     bailoutPattern: "(?<!\\\\)\n",
     deleteMatchRules: true,
-    newFileLocation: argv[3]
-});
+    newFileLocation: newFileLocation,
+})
+
+const grammarObject = JSON.parse(fs.readFileSync(newFileLocation))
+// remove the file types from the generated grammar
+grammarObject["fileTypes"] = []
+fs.writeFileSync(newFileLocation, JSON.stringify(grammarObject))
