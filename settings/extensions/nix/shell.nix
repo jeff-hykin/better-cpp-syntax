c1d34f28e4d245d31fa1f74dc7da95a531feb2ea
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-19 09:51:37 -0600

    add salt

--- settings/extensions/nix/shell.nix
+++ settings/extensions/nix/shell.nix
@@ -21,6 +21,11 @@ let
         )
     );
     
+    salt = (builtins.import
+        (./nixpkgs/salt.nix)
+        main
+    );
+    
     # just a helper
     emptyOptions = ({
         buildInputs = [];
@@ -65,7 +70,7 @@ in
     # 
     main.packages.mkShell {
         # inside that shell, make sure to use these packages
-        buildInputs =  main.project.buildInputs ++ macOnly.buildInputs ++ linuxOnly.buildInputs;
+        buildInputs =  main.project.buildInputs ++ macOnly.buildInputs ++ linuxOnly.buildInputs ++ [ salt ];
         
         nativeBuildInputs =  main.project.nativeBuildInputs ++ macOnly.nativeBuildInputs ++ linuxOnly.nativeBuildInputs;
         
