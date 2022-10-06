e5a21177aaf24c1e9ec2f2b988b6bdcb4d106f33
Jeff Hykin <jeff.hykin@gmail.com>
2022-10-06 11:37:17 -0500

    simplify inline comment, no measurable performance effect

--- language_examples/#287.cpp
+++ language_examples/#287.cpp
@@ -1,3 +1,6 @@
 a ********
 a &&&&&&&&&& 
 "***** Program Info ********" 
+char * string = "a string; buggy ***************";
+DEBUG_UART_PORT.print( F( "\r******************* Program Info *******************\r\n" ) );
+/**/
