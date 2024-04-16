0972e687abb211938322507670b77764d123ac36
Jeff Hykin <jeff.hykin@gmail.com>
2024-04-16 08:47:59 -0500

    fix #653

--- main/patterns/preprocessor.rb
+++ main/patterns/preprocessor.rb
@@ -32,6 +32,7 @@ grammar.exports = [
     :preprocessor_conditional_parentheses,
     :preprocessor_conditional_standalone,
     :preprocessor_context,
+    :preprocessor_number_literal, # NOTE: this shouldn't need to be exported, but there's a bug in ruby_grammar_builder (see https://github.com/jeff-hykin/better-cpp-syntax/issues/653)
 ]
 
 std_space = grammar[:std_space]
